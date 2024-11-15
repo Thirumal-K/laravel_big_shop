31901c385b2d5118d1b72486102c8a6e47f79d8d
Thirumal-k <smaruthi030@gmail.com>
2024-11-15 14:19:41 +0530

    category resource added

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use App\Enums\CategoryStatus;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationLable = 'product Category';
+    
+    protected static ?string $navigationGroup = 'Ecommerce';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('description')
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('image_path')
+                    ->image()
+                    ->directory('category'),
+                Forms\Components\TextInput::make('buying_price')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('selling_price')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('excepted_profit')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('eraned_profit')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('company')
+                    ->numeric(),
+                Forms\Components\Select::make('status')
+                    ->options(CategoryStatus::Class)
+                    ->default(CatagoryStatus::PUBLISH),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                    Tables\Columns\TextColumn::make('product_count')
+                    ->getStateUsing(fn (Category $category): string => $category->products()->count())
+                    ->searchable(),  
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image_path')
+                ->getStateUsing(fn (Category $record): string => $record->GetLogoImage())
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('buying_price')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('selling_price')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('excepted_profit')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('eraned_profit')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('company')
+                    ->numeric()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable()
+                    ->badge()
+                    ->color(fn(string $state):  string => match ($state) {
+                        CategoryStatus::PUBLISH->value => 'success',
+                        CategoryStatus::DRAFT->value => 'warning',                        
+                        CategoryStatus::PENDING->value => 'danger',
+                    }),
+                Tables\Columns\TextColukmn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
