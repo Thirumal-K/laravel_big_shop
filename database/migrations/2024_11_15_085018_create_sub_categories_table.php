6506c9667e007522b5d6424ac9ef87ffe3d389b2
Thirumal-k <smaruthi030@gmail.com>
2024-11-15 15:05:53 +0530

    update subcategory

--- database/migrations/2024_11_15_085018_create_sub_categories_table.php
+++ database/migrations/2024_11_15_085018_create_sub_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use App\Enums\SubCategory\Status;
+use App\Enums\SubCategoryStatus;
 return new class extends Migration
 {
     /**
