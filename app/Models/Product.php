eca89fcc98c31df40020975044293e894c8761b6
Thirumal-k <smaruthi030@gmail.com>
2024-11-16 19:10:05 +0530

    product  model and migration added

---
+++ app/Models/Product.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Facades\Storage;
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'image_path',
+        'price',
+        'category_id',
+        'brand_id',
+        'qty',
+        'alert_stock',
+        'description',
+        'product_label_id',
+        'product_tag_id',
+        'stock_status',
+        'is_featured',
+        'min_order_qty',
+        'max_order_qty'
+    ];
+    Protected$casts =[
+        'image_path' =>'array',
+    ];
+
+    public function brand()
+    {
+       return $this->belongTo(Brand::class,'brand_id');
+    }
+     // public function category():
+    // {
+    //     return $this->belongsTo(Category::class,'category_id');
+    // }
+
+    public function category()
+    {
+        return $this->belongsToMany(Category::class, 'product_categories');
+    }
+
+    public function productLabel()
+    {
+        return $this->belongsToMany(Label::class,'product_labels');
+    }
+
+    public function productTag()
+    {
+        return $this->belongsToMany(Tag::class,'product_tags');
+    }
+
+    public function productCollection()
+    {
+        return $this->belongsToMany(Collection::class,'product_collections');
+    }
+    
+    public function productCompany()
+    {
+        return $this->belongsToMany(Company::class,'product_companies');
+    }
+
+
+    public function GetImagePath()
+    {
+        if (is_array($this->image_path)) {
+            return array_map(function ($path) {
+                return env('DOMAIN_URL') . Storage::url($path);
+            }, $this->image_path);
+        }
+        return env('DOMAIN_URL') . Storage::url($this->image_path);
+    }
+
+    public function DeleteLogoImage()
+    {        
+        if (Storage::exists($this->image_path)) 
+        {
+            Storage::delete($this->image_path);
+        }
+    }
+
+}
---
+++ database/migrations/2024_11_16_132316_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Enums\StockStatus;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('description')->nullable();
+            $table->decimal('price',10,2)->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->unsignedBigInteger('product_label_id')->nullable();
+            $table->unsignedBigInteger('product_tag_id')->nullable();
+            $table->string('image_path')->default('no_image_available.jpg')->nullable();
+            $table->integer('qty')->nullable();
+            $table->integer('alert_stock')->default(0);
+            $table->string('stock_status')->default(StockStatus::InStock)->nullable();
+            $table->string('is_featured')->nullable()->default(false);
+            $table->string('min_order_qty')->nullable()->default(0);
+            $table->string('max_order_qty')->nullable()->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
