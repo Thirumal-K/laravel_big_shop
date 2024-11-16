99d93088632068e2970235c11d81e3f1a1494340
Thirumal-k <smaruthi030@gmail.com>
2024-11-16 20:09:02 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -29,11 +29,11 @@ class Product extends Model
         'image_path' =>'array',
     ];
 
-    public function brand()
-    {
-       return $this->belongsTo(Brand::class,'brand_id');
-    }
-     // public function category():
+    // public function brand()
+    // {
+    //    return $this->belongsTo(Brand::class,'brand_id');
+    // }
+    //  // public function category():
     // {
     //     return $this->belongsTo(Category::class,'category_id');
     // }
