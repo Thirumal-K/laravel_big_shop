3ed004321ab55026d891e0b63bac057523d0676f
Thirumal-k <smaruthi030@gmail.com>
2024-11-22 19:19:14 +0530

    Create LabelSeeder.php

---
+++ database/seeders/LabelSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Label;
+
+class LabelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $product_labels=[
+            [
+                'name'=> 'Hot',
+                'color'=>'#e74c3c'
+            ],
+            [
+                'name'=>'New',
+                'color'=>'#2ecc71'
+            ],
+            [
+                'name'=>'Sale',
+                'color'=>'#8e44ad'
+            ]
+
+        ];
+        
+        foreach ($product_labels as $row){
+            Label::create($row);
+        }
+    }
+}
